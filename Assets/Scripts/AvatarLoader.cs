50a7071f91629d16c21e27b93b271ce3d3bb81df
Robin Sharma <robin@wolf3d.io>
2022-08-30 22:18:02 +0200

    Add doxygen action

---
+++ Assets/Scripts/AvatarLoader.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class AvatarLoader
+{
+    /// <summary>
+    /// Url for downloading avatar
+    /// </summary>
+    public string Url;
+
+    // Loading time.
+    private float loadTime;
+
+    /// <summary>
+    /// Loads avatar.
+    /// </summary>
+    public void Load()
+    {
+    }
+
+    /// <summary>
+    /// Unloads avatar.
+    /// </summary>
+    public void Unload()
+    {
+    }
+
+    private void DoSomethingWhileLoading()
+    {
+    }
+}
